0ff01450c44925d488d1002a50cd922a71a6ed82
David Kemp <19152940+davaya@users.noreply.github.com>
2025-05-08 14:29:17 -0400

    reader

--- get-iso3166.py
+++ get-iso3166.py
@@ -1,75 +1,63 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from bs4 import BeautifulSoup
-
-# import pandas as pd
-# import html
-# import datetime
 
 
 def initialize_driver():
     options = webdriver.ChromeOptions()
-    # options.add_argument('--headless=new')
+    options.add_argument('--headless=new')
     options.add_argument('--disable-gpu')
     return webdriver.Chrome(options=options)
 
 
-def get_product_data(driver, url):
+def get_country_data(driver, url):
     driver.implicitly_wait(5)
     driver.get(url)
-    header = driver.find_elements(By.CLASS_NAME, "core-view-line")
-    h = header.get_attribute('outerHTML')
-    # hsoup = BeautifulSoup(header.get_attribute('outerHTML'), features='html.parser')
-    # he = header.find_elements(By.XPATH, './/*')   # recursive
-    # he = header.find_elements(By.XPATH, './child::*')   # first level
-    print(f'header "{driver.title}"')
-    content = driver.find_element(By.ID, "country-additional-info")
-    # csoup = BeautifulSoup(content.get_attribute('outerHTML'), features="html.parser")
+    summary = []
+    for line in driver.find_elements(By.CLASS_NAME, "core-view-line"):
+        summary.append([v.text for v in line.find_elements(By.XPATH, './/*')])
 
-"""
-    products, prices, currencies, ratings = [], [], [], []
+    inf = driver.find_element(By.ID, 'country-additional-info')
+    info = [[v.text for v in inf.find_elements(By.TAG_NAME, 'th')]]
+    body = inf.find_element(By.TAG_NAME,'tbody')
+    for e in body.find_elements(By.TAG_NAME, 'tr'):
+        info.append([v.text for v in e.find_elements(By.TAG_NAME, 'td')])
 
-    for a in soup.findAll('a', href=True, attrs={'class': 'ratio-box product-link'}):
-        name = a.find('h4', attrs={'class': 'name'})
-        price = a.find('div', attrs={'class': 'price'})
-        currency, amount = html.unescape(price.text).split()
-        rating = a.find('span', attrs={'class': 'product-rating-count'})
+    sub = driver.find_element(By.ID, "subdivision")
+    subdivisions = [[v.text for v in sub.find_elements(By.TAG_NAME, 'th')]]
+    body = sub.find_element(By.TAG_NAME,'tbody')
+    for e in body.find_elements(By.TAG_NAME, 'tr'):
+        subdivisions.append([v.text for v in e.find_elements(By.TAG_NAME, 'td')])
 
-        prices.append(amount.strip())
-        currencies.append(currency.strip())
-        products.append(name.text.strip())
-        ratings.append(rating.text.strip().strip('()'))
+    # X.find_elements(By.XPATH, './/*')         # recursive
+    # X.find_elements(By.XPATH, './child::*')   # first level
+    # Y = BeautifulSoup(X.get_attribute('outerHTML'), features='html.parser')
 
-    return products, prices, currencies, ratings
-"""
+    return summary, info, subdivisions
+
 
 def main():
-    # url = "https://uae.sharafdg.com/c/computing/laptops/macbooks/"
-    # url = "https://www.iso.org/obp/ui/#iso:pub:PUB500001:en"
-    url = "https://www.iso.org/obp/ui/#iso:code:3166:US"
+    # url = "https://www.iso.org/obp/ui/#iso:pub:PUB500001:en"  # List of countries (3166-1)
+    url = "https://www.iso.org/obp/ui/#iso:code:3166:US"    # List of subdivisions within a country (3166-2)
 
     with initialize_driver() as driver:
-        products, prices, currencies, ratings = get_product_data(driver, url)
+        summary, info, subdivisions = get_country_data(driver, url)
+        title = driver.title
 
-"""
-        df = pd.DataFrame({
-            'Product Name': products,
-            'Price': prices,
-            'Currency': currencies,
-            'Rating': ratings
-        })
+    print(f'{len(subdivisions)} ISO 3166-2 Subdivisions in "{title}"')
 
-        print(f'dataframe')
+    """
+    for v in summary:
+        assert len(v) == 2
+        print(f'{v[0]:>25}: {v[1]}')
 
-        current_date = datetime.date.today()
-        df['year'] = current_date.year
-        df['month'] = current_date.month
+    print('\nAdditional Info:')
+    for v in info:
+        assert len(v) == 3
+        print(f'  {v}')
+    """
+    for n, s in enumerate(subdivisions, start=1):
+        print(f'{n:>4} {s}')
 
-        df.to_csv('products.csv', index=False, encoding='utf-8')
-        df.to_parquet('data.parquet', engine='pyarrow',
-                      partition_cols=['year', 'month'])
-"""
 
 if __name__ == "__main__":
     main()

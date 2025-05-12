d8ccde4936c8e7879e31d13b2ffb9e60ef260ae4
David Kemp <19152940+davaya@users.noreply.github.com>
2025-05-12 16:21:56 -0400

    make jadn

--- make-jadn.py
+++ make-jadn.py
@@ -7,7 +7,12 @@ def main():
         ccodes = json.load(fp)
     print(len(ccodes))
     col = {ccodes[0][n]: n for n in range(len(ccodes[0]))}
-    pass
+
+    fields = [[int(cc[col['Numeric']]), cc[col['Alpha-2 code']], cc[col['English short name']]] for cc in ccodes[1:]]
+    types = [['ISO-3166-1', 'Enumerated', [], 'Country codes'], fields]
+
+    with open('iso-3166-1.jadn', 'w') as fp:
+        json.dump({'meta': {'package': 'http://iso.org/3166-1'}, 'types': types}, fp)
 
 if __name__ == "__main__":
     main()
